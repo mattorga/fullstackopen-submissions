8710a838e0dde7a4b6f2265959f4ace48d878737
mattorga <mattheworga85@gmail.com>
2024-07-02 10:54:39 +0800

    Exercise 2.11 - Added Effect Hook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({value, onChange}) => {
   return (
@@ -27,21 +28,21 @@ const PersonForm = (props) => {
   )
 }
 
-// const Persons = ({name, number, id}) => <p>{name} {number}</p>
-
 const Persons = ({persons}) => persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('') // For controlling form element
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
   const [showAll, setShowAll] = useState('') // False - filter has value
+
+  useEffect(() => {axios
+    .get('http://localhost:3001/persons')
+    .then(response =>{
+      setPersons(response.data)
+    })
+  },[])
 
   const addPerson = (event) => {
     event.preventDefault()
