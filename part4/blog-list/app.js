e17a19f5dd80796edbae02b503dc997b4ed5329d
Matthew Orga <mattheworga85@gmail.com>
2025-10-31 22:52:12 +0800

    Refactored

---
+++ part4/blog-list/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express();
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB') // Convert to logger
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB', error.message) // Convert to logger
+  });
+
+// app.use(express.static('dist')) 
+app.use(express.json());
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
